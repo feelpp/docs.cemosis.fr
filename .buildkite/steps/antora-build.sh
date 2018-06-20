b7103298e4ceb5c1f6450d17a329340bc80d811f
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-06-20 10:44:04 +0200

    up

--- .buildkite/steps/antora-build.sh
+++ .buildkite/steps/antora-build.sh
@@ -7,8 +7,9 @@ BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
 echo "--- Building $PROJECT..."
 #git clone git@github.com:feelpp/swimmer.git toto
 
-antora --pull --html-url-extension-style=indexify site.yml
+echo "running $NVM_BIN/antora..."
+$NVM_BIN/antora --pull --html-url-extension-style=indexify site.yml
 #ls -lrta
-
+echo "uploading artifact..."
 #tar  --exclude='*.png' --exclude="*.jpg" --exclude="*.jpeg" -c -z -f site.tar.gz build
 buildkite-agent artifact upload site.tar.gz --job $BUILDKITE_JOB_ID
