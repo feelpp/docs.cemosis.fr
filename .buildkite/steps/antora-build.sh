c1ba7b4290e0b790a52677ced054807d25a931d9
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2020-08-13 13:05:41 +0200

    create a package.json Fixes  #3

--- .buildkite/steps/antora-build.sh
+++ .buildkite/steps/antora-build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 set -eo pipefail
-set -x
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#set -x
 
 BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
-echo "--- Building $PROJECT..."
-#git clone git@github.com:feelpp/swimmer.git toto
+echo "--- Install"
+npm install
+echo "--- Build"
+npm run build
+echo "--- Deploy"
+rsync -avz --delete build/site/ $WEBSERVER:~/public_html/$PROJECT
 
-echo "running $NVM_BIN/antora..."
-$NVM_BIN/antora --fetch --html-url-extension-style=indexify site.yml
-#ls -lrta
-echo "uploading artifact..."
-tar  -c -z -f site.tar.gz build
-buildkite-agent artifact upload site.tar.gz --job $BUILDKITE_JOB_ID
+#echo "running $NVM_BIN/antora..."
+#$NVM_BIN/antora --fetch --html-url-extension-style=indexify site.yml
+##ls -lrta
+#echo "uploading artifact..."
+#tar  -c -z -f site.tar.gz build
+#buildkite-agent artifact upload site.tar.gz --job $BUILDKITE_JOB_ID
